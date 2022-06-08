ddb8217ff1dd539578a5ee0b9a9e7d1168180668
berg <605908930@qq.com>
2022-06-08 17:19:33 +0800

    add limit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/", async (req, res) => {
   // res.sendFile(path.join(__dirname, "index.html"));
   let requestData = {
     env: "training-kp81r",
-    query: 'db.collection("forW").limit(10).skip(1).get()',
+    query: 'db.collection("forW").limit(100).skip(1).get()',
   };
   // request('http://api.weixin.qq.com/wxa/getwxadevinfo', console.log);
   request(
